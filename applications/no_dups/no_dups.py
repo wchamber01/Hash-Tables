679bd634dad996f4829e215f5903bb299f2c8bae
wchamber01 <williamchambers@gmail.com>
2020-05-07 23:14:29 -0400

    no_dupes

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,28 @@
 def no_dups(s):
     # Implement me.
+    # split string into list of words
+    s_list = s.split()
+    hash_table = {}
+    prev = None
+    # iterate through list
+    for word in s_list:
+        if prev not in hash_table:
+            if prev != word:
+                if word not in hash_table:
+                    hash_table[prev] = word
+                    prev = word
+    # chain None to end
+    hash_table[prev] = None
+    # loop through and create string with key, values
+    no_dups_s = ''
+    word = hash_table[None]
+    while word != None:
+        no_dups_s += word
+        word = hash_table[word]
+        if word is not None:
+            no_dups_s += ' '
+
+    return no_dups_s
 
 
 if __name__ == "__main__":
