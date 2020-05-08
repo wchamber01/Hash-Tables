1902b5e34a3c7bc097f32abf064571af5953016f
wchamber01 <williamchambers@gmail.com>
2020-05-07 23:03:29 -0400

    completed lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,18 +1,30 @@
 import math
 import random
 
+
 def slowfun(x, y):
     # TODO: Modify to produce the same results, but much faster
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
+    # v = math.pow(x, y)
+    # v = math.factorial(v)
+    # v //= (x+y)
+    # v %= 982451653
+    # return v
 
-    return v
+    cache = {}
+
+    def factorial(x, y):
+        if (x, y) not in cache:
+            v = math.pow(x, y)
+            v = math.factorial(v)
+            v //= (x+y)
+            v %= 982451653
+            cache[str(x+y)] = v
+            print(cache[str(x+y)])
+        return str(cache[str(x+y)])
+    return factorial
 
 
 # Do not modify below this line!
-
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
